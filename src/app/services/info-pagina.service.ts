156c18676a36d275efcad5b955c2c8cd235de141
Christian <christiancruzarango@gmail.com>
2019-06-23 13:01:01 -0500

    final seccion 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,19 +9,34 @@ export class InfoPaginaService {
 
   info:InfoPagina = {};
   cargada=false;
+  equipo: any[] = [];
 
   constructor(private http:HttpClient) {
 
-    console.log('Servicio de infopagina listo');
+    this.cargarInfo();
+    this.cargarEquipo();
+   }
 
-    //Leer archivo json que se encuentra en assets/data/data-pagina.json
+   private cargarInfo(){
+      //Leer archivo json que se encuentra en assets/data/data-pagina.json
     /*para mostrar las propiedades personalizables ejemplo resp.email se debe encerrar la respuesta entre()
     y colocarle el tipo de dato que en este caso es la interfaces */
     this.http.get('assets/data/data-pagina.json')
              .subscribe((resp:InfoPagina) => {
               this.cargada=true;
               this.info=resp;
-                console.log(resp);
-             })
+                //console.log(resp);
+             });
    }
+
+   private cargarEquipo(){
+    this.http.get('https://angular-html-7b738.firebaseio.com/equipo.json')
+          .subscribe((resp:any[])=>{
+            this.equipo = resp;
+            console.log(resp);
+          });
+   }
+
 }
+
+
